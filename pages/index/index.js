4b2fccfa3a88da69c1393d7cb75ed37d30d20bd1
肖若轩 <xiaoruoxuan@uce.cn>
2019-07-30 18:01:57 +0800

    项目初始化

---
+++ pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+	data: {
+		Keyword:['华为手机','小米手机','oppo手机','黑鲨手机']
+	},
+	onLoad: function () {
+		
+	},
+	TZlogs:function(){
+		wx.switchTab({
+			url: '../logs/logs'
+		})
+	},
+	TZsearch:function(){
+		wx.switchTab({
+			url: '../search/search'
+		})
+	},
+	onPullDownRefresh: function () {
+		wx.showNavigationBarLoading() //在标题栏中显示加载
+		setTimeout(function () {
+			wx.hideNavigationBarLoading() //完成停止加载
+			wx.stopPullDownRefresh() //停止下拉刷新
+		}, 1500);
+		this.setData({
+			Keyword: ['锤子手机', 'E+手机', '三星手机', '游戏手机']
+		})
+	},
+})
